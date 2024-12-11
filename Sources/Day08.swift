ebe5c74e77b7c752e21d7928942e28c1c5fb3a01
Sam Johnson <25811+samwich@users.noreply.github.com>
2024-12-11 15:09:27 -0800

    Day 8, Part 2

--- Sources/Day08.swift
+++ Sources/Day08.swift
@@ -69,9 +69,41 @@ struct Day08: AdventDay {
       return [Coordinate(x: cx, y: cy), Coordinate(x: dx, y: dy)]
     }
 
+    func lineOfAntinodesForPair(a: Coordinate, b: Coordinate) -> [Coordinate] {
+      let xDiff = a.x - b.x
+      let yDiff = a.y - b.y
+
+      var antinodes: [Coordinate] = []
+      // start at a
+      var x = a.x
+      var y = a.y
+
+      // keep subtracting the diff until we're out of bounds
+      while inBounds(coordinate: Coordinate(x: x, y: y)) {
+        antinodes.append(Coordinate(x: x, y: y))
+        x -= xDiff
+        y -= yDiff
+      }
+      // restart at a
+      x = a.x
+      y = a.y
+      // keep adding the diff until we're out of bounds
+      while inBounds(coordinate: Coordinate(x: x, y: y)) {
+        antinodes.append(Coordinate(x: x, y: y))
+        x += xDiff
+        y += yDiff
+      }
+
+      return antinodes
+    }
+
     func pairsForAntenna(character: String) -> [[Coordinate]] {
       let coords = coordinatesByCharacter[character]!
       return Array(coords.combinations(ofCount: 2))
+    }
+
+    func inBounds(coordinate: Coordinate) -> Bool {
+      (0...grid.maxPoint.x).contains(coordinate.x) && (0...grid.maxPoint.y).contains(coordinate.y)
     }
 
     func antinodes() -> [Coordinate] {
@@ -83,11 +115,21 @@ struct Day08: AdventDay {
           antinodes.append(contentsOf: antinodesForPair(a: pair[0], b: pair[1]))
         }
       }
-      let inBoundsAntinodes = antinodes.filter {
-        (0...grid.maxPoint.x).contains($0.x) && (0...grid.maxPoint.y).contains($0.y)
-      }
+      let inBoundsAntinodes = antinodes.filter { inBounds(coordinate: $0) }
       // Use a set to ignore duplicates
       return Array(Set(inBoundsAntinodes))
+    }
+
+    func antinodesPart2() -> [Coordinate] {
+      var antinodes: [Coordinate] = []
+
+      for char in coordinatesByCharacter.keys {
+        for pair in pairsForAntenna(character: char) {
+          antinodes.append(contentsOf: lineOfAntinodesForPair(a: pair[0], b: pair[1]))
+        }
+      }
+      // Use a set to ignore duplicates
+      return Array(Set(antinodes))
     }
 
   }
@@ -113,6 +155,6 @@ struct Day08: AdventDay {
   }
 
   func part2() -> Any {
-    return 0
+    return antennaMap.antinodesPart2().count // 1293
   }
 }
