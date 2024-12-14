cc688e2b111b29ba6dd6ffa79bf9f7306d7c9fc0
Sam Johnson <25811+samwich@users.noreply.github.com>
2024-12-14 11:03:46 -0800

    Day 11, Part 2

--- Sources/Day11.swift
+++ Sources/Day11.swift
@@ -30,7 +30,7 @@ struct Day11: AdventDay {
   func play1(steps: Int, stones: [Int]) -> [Int] {
     var stepStones = stones
     for i in 0..<steps {
-      print("Beginning iteration \(i)")
+//      print("Beginning iteration \(i)")
       stepStones = blink1(stones: stepStones)
 //      print("i = \(i)")
 //      print(stepStones)
@@ -43,8 +43,73 @@ struct Day11: AdventDay {
     return play1(steps: 25, stones: stones).count // 233875
   }
 
+  var entities2: [Substring] {
+    data.trimmingCharacters(in: .whitespacesAndNewlines).split(separator: " ")
+  }
+
+  class StoneCounter: @unchecked Sendable {
+    var stones: [String: Int]
+
+    init(input: [Substring]) {
+      var stones: [String: Int] = [:]
+      for s in input {
+        stones[String(s)] = 1
+      }
+      self.stones = stones
+    }
+
+    var count: Int {
+      stones.values.reduce(0, +)
+    }
+
+    func play(steps: Int) {
+      var playStones = stones
+      for _ in 0..<steps {
+        playStones = blink(playStones)
+      }
+      self.stones = playStones
+    }
+
+    func blink(_ stones: [String: Int]) -> [String: Int] {
+      let keys = stones.keys
+      var blinkStones: [String: Int] = [:]
+      for k in keys {
+        let stoneCount = stones[k]!
+        if stoneCount == 0 { continue }
+
+        // transform k
+        let transformed = transform(stone: Int(k)!)
+        // we're replacing all of k with the transformed values
+//        blinkStones[k] = 0 // for some reason, this is ruining the results
+        // store the results
+//        print("\(k) became \(transformed)")
+        for t in transformed {
+          blinkStones[String(t), default: 0] += stoneCount
+        }
+      }
+//      print("blinkStones == stones: \(blinkStones == stones)")
+      return blinkStones
+    }
+
+    func transform(stone: Int) -> [Int] {
+      if stone == 0 {
+        return [1]
+      } else if String(stone).count.isMultiple(of: 2) {
+        let s = String(stone)
+        let halfCount = s.count / 2
+        let first = Int(s.dropLast(halfCount))!
+        let second = Int(s.dropFirst(halfCount))!
+        return [first, second]
+      } else {
+        return [stone * 2024]
+      }
+    }
+
+  }
+
   func part2() -> Any {
-    let stones = entities
-    return play1(steps: 75, stones: stones).count // 233875
+    let counter = StoneCounter(input: entities2)
+    counter.play(steps: 75)
+    return counter.count // 277444936413293
   }
 }
--- Tests/Day11.swift
+++ Tests/Day11.swift
@@ -15,8 +15,8 @@ struct Day11Tests {
     #expect(String(describing: challenge.part1()) == "55312")
   }
 
-//  @Test func testPart2() async throws {
-//    let challenge = Day11(data: testData)
-//    #expect(String(describing: challenge.part2()) == "part2")
-//  }
+  @Test func testPart2() async throws {
+    let challenge = Day11(data: testData)
+    #expect(String(describing: challenge.part2()) == "65601038650482")
+  }
 }
